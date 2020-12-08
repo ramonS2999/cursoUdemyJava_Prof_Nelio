65dc33d484ba0a98fd3d3bc53a399809fc596637
Luis Ramon <ramonlima3897@gmail.com>
2020-12-08 13:34:37 -0300

    Uma tarefa de polimorfismo

---
+++ tarefa09/entities/OutsourcedEmployee.java
@@ -0,0 +1,29 @@
+package tarefa09.entities;
+
+public class OutsourcedEmployee extends Employee{
+    
+    private final Double taxPerCent = 1.1;
+    private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+        super();
+    }
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    } 
+    
+    @Override
+    public double payment() {
+        return super.payment() + additionalCharge * taxPerCent;
+    }
+}
