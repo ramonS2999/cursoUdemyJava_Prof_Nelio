b2ca0232bd6529609d08c5af3cc202730909fa99
Luis Ramon <ramonlima3897@gmail.com>
2020-12-10 19:03:46 -0300

    Good solution

--- exercicio10/app/Program.java
+++ exercicio10/app/Program.java
@@ -6,9 +6,10 @@ import java.util.Date;
 import java.util.Scanner;
 
 import exercicio10.model.entities.Resevation;
+import exercicio10.model.exceptions.DomainException;
 
 public class Program {
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -16,18 +17,16 @@ public class Program {
         System.out.print("Roon number: ");
         int number = sc.nextInt();
 
-        System.out.print("Check-in date (dd/MM/yyyy): ");
-        Date checkIn = sdf.parse(sc.next());
-
-        System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date checkOut = sdf.parse(sc.next());
-
-        if (!checkOut.after(checkIn)) {
-            System.out.println("Error in resevation: Check-out date must be after check-in date");
-        } else {
+        try {
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            Date checkIn = sdf.parse(sc.next());
+    
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            Date checkOut = sdf.parse(sc.next());
+        
             Resevation resevation = new Resevation(number, checkIn, checkOut);
             System.out.println("Resevation: " + resevation);
-
+    
             System.out.println();
             System.out.println("Enter data to upadate the resevation: ");
             System.out.print("Check-in date (dd/MM/yyyy): ");
@@ -35,14 +34,18 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
             
-            String error = resevation.updateDates(checkIn, checkOut);
-            if (error != null) {
-                System.out.println("Error in resevation: " + error);
-            }
-            else {
-                System.out.println("Resevation: " + resevation);                
-            }
-
+            resevation.updateDates(checkIn, checkOut);
+            System.out.println("Resevation: " + resevation);                
+            
+        } 
+        catch (ParseException e) {
+            System.out.println("Invalide date fromat");
+        }
+        catch (DomainException e) {
+            System.out.println("Error in reservation: " + e.getMessage());
+        }
+        catch (RuntimeException e) {
+            System.out.println("Unexpected error");
         }
 
         sc.close();
--- exercicio10/model/entities/Resevation.java
+++ exercicio10/model/entities/Resevation.java
@@ -4,8 +4,12 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-public class Resevation {
+import org.w3c.dom.DOMException;
 
+import exercicio10.model.exceptions.DomainException;
+
+public class Resevation {
+    
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
@@ -15,7 +19,10 @@ public class Resevation {
     public Resevation() {
     }
 
-    public Resevation(Integer roomNumber, Date checkIn, Date checkOut) {
+    public Resevation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.roomNumber = roomNumber;
         this.checkIn = checkIn;
         this.checkOut = checkOut;
@@ -42,20 +49,19 @@ public class Resevation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public String updateDates(Date checkIn, Date checkOut) {
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 
         Date now = new Date();
         if (checkIn.before(now) || checkOut.before(now)) {
-            return "Resevation dates for update must be future dates";
+            throw new DomainException("Resevation dates for update must be future dates");
 
         }
         if (!checkOut.after(checkIn)) {
-            return "Check-out date must be after check-in date";
+            throw new DomainException("Check-out date must be after check-in date");
         }
 
         this.checkIn = checkIn;
         this.checkOut = checkOut;
-        return null;
     }
 
     @Override
